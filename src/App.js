3a574ac5a12ca4d56db56b3d457fa05c99cad818
Cristian Alinez <cristian.alinez@pjn.gov.ar>
2026-04-22 15:07:56 -0300

    andando

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { useEffect, useState } from 'react';
 import alchemy from './Api.js'
+import Block from './Block.js';
+import {Container} from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-
-
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 
 function App() {
@@ -23,7 +39,11 @@ function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      {/* The rest of your application */}
+      <ThemeProvider theme={theme}>
+        <Container sx={{ bgcolor: 'primary.main', height: '100vh', }}>
+          <Block />
+        </Container>
+      </ThemeProvider>
     </React.Fragment>
   );
 }
--- src/Block.js
+++ src/Block.js
@@ -1,14 +1,30 @@
+import {Grid } from '@mui/material';
+import alchemy  from './Api.js'
+import { useEffect, useState } from 'react';
+
+
 function Block (){
+    const client = alchemy;
+    const [block, setBlock] = useState(null);
 
-    function gerBlockDetail(){
+    useEffect(() => {
+        const getBlock = async () => {
+            const blockNumber = await client.core.getBlockNumber();
+            const block_ = await client.core.getBlock(blockNumber);
+            console.log(block_);
+            setBlock(block_);
+        };
 
-    }
+        getBlock();
+    }, []);
 
     return (
-        <>
-            
+       <Grid container>
+            <Grid size={8}>{
+                block ? JSON.stringify(block) : "Loading..."
+            }</Grid>
 
-        </>
+       </Grid >
     );
 }
 
